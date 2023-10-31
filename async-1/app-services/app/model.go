5f97bfee7bf72910e74cda560973efa0ef101e2e
ibnu irmanto <ibnu19@gmail.com>
2023-10-31 07:59:23 +0700

    Delete async-1 directory

--- async-1/app-services/app/model.go
+++
@@ -1,8 +0,0 @@
-package app
-
-type Email struct {
-	To      []string `json:"to"`
-	Subject string   `json:"subject"`
-	Message string   `json:"message"`
-	Type    string   `json:"type"`
-}
--- async-1/mail-services/app/helper.go
+++
@@ -1,8 +0,0 @@
-package app
-
-func ConvertToStruct(request EmailRequest, email *Email) {
-	email.To = request.To
-	email.Subject = request.Subject
-	email.Message = request.Message
-	email.Type = request.Type
-}
